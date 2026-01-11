f563d8995f7dc6358bd69f0a53090d6f710a8f13
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-01-11 09:30:25 +0530

    Update solution: 1-TwoSum.c

--- easy/1-TwoSum.c
+++ easy/1-TwoSum.c
@@ -4,13 +4,4 @@
  * Date: 1/11/2026
  */
 
-        // Store numbers with their original indices
-        for (int i = 0; i < n; i++)
-        int n = nums.size();
-        vector<pair<int, int>> numWithIndex;
-
-public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-class Solution 
-{
+c
